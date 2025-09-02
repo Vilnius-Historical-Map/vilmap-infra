fde138c68f44e13cc848fefbcea5163d7160c6a1
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-09-02 16:26:00 +0300

    feature: add bastion-sg

---
+++ infra/bastion-sg.tf
@@ -0,0 +1,14 @@
+module "bastion-sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+  name        = "${project-name}-bastion-sg"
+  description = "Security group for bastion"
+  vpc_id      = module.vpc.vpc_id
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
