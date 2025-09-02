27c0465c97ebb69b17095ce72c23e3159f48381d
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-09-02 16:25:52 +0300

    feature: add rds-sg

---
+++ infra/rds-sg.tf
@@ -0,0 +1,23 @@
+module "rds-sg" {
+  source      = "terraform-aws-modules/security-group/aws"
+  name        = "${project-name}-rds-sg"
+  description = "Security group for RDS database"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_source_security_group_id = [
+    {
+      from_port                = 5432
+      to_port                  = 5432
+      protocol                 = "tcp"
+      source_security_group_id = module.bastion-sg.security_group_id
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
