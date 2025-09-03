8ec78f3cc45c6d99b11d3aabce20fcd2c4441f23
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-09-03 14:41:42 +0300

    refactor: parameterize rds

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,29 +1,26 @@
 module "rds" {
-  source = "terraform-aws-modules/rds/aws"
-  identifier = "vilmapdb"
+  source     = "terraform-aws-modules/rds/aws"
+  identifier = var.rds_identifier
 
-  major_engine_version                = "17"
+  major_engine_version                = var.rds_engine_version
   auto_minor_version_upgrade          = true
-  instance_class                      = "db.t3.micro"
-  allocated_storage                   = 10
-  family                              = "postgres17"
-  port                                = "5432"
+  instance_class                      = var.rds_instance_class
+  allocated_storage                   = var.rds_allocated_storage
+  family                              = var.rds_family
+  port                                = var.rds_port
   iam_database_authentication_enabled = false
 
   manage_master_user_password = false
-  password                    = data.aws_ssm_parameter.db_password
-  username                    = data.aws_ssm_parameter.db_username
-  db_name                     = "vilmapDB"
+  password                    = data.aws_ssm_parameter.db_password.value
+  username                    = data.aws_ssm_parameter.db_username.value
+  db_name                     = var.rds_db_name
 
-  monitoring_role_name   = "VilmapRDSMonitoringRole"
+  monitoring_role_name   = var.rds_monitoring_role_name
   create_monitoring_role = true
 
   create_db_subnet_group = true
-  subnet_ids = [module.vpc.private_subnets]
+  subnet_ids             = module.vpc.private_subnets
   deletion_protection    = true
 
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
+  tags = var.tags
 }
