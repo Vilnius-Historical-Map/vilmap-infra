619e06cfcae6c24c8582fb5e0be9ed9da8e88a1e
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-09-03 14:42:16 +0300

    refactor: parameterize bastion and add iam role for ssm

--- infra/bastion.tf
+++ infra/bastion.tf
@@ -1,15 +1,17 @@
-module "ec2_instance" {
+module "bastion" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "${project-name}-bastion"
+  name = "${var.project_name}-bastion"
 
-  instance_type = "t3.micro"
+  instance_type = var.bastion_instance_type
   subnet_id     = module.vpc.public_subnets[0]
 
-#   create_iam_instance_profile = true
+  create_iam_instance_profile = true
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
+  iam_role_name = "${var.project_name}-bastion-role"
+  iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   }
+
+  tags = var.tags
 }
