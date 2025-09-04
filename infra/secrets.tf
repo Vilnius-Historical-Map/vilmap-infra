d78e6ee5ad7e03758aa843685204c4411c9c9461
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-09-04 12:29:06 +0300

    fix: provide right name for db secrets

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,9 +1,9 @@
 data "aws_ssm_parameter" "db_username" {
-  name            = "${var.project_name}/prod/db-username"
+  name            = "/${var.project_name}/prod/db-user"
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "db_password" {
-  name            = "${var.project_name}/prod/db-password"
+  name            = "/${var.project_name}/prod/db-password"
   with_decryption = true
 }
