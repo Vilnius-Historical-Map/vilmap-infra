17a1229ffbe21a412a4dbe1d847eff4fe68da751
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-09-08 16:23:13 +0300

    refactor: add lifecycle policy to s3

--- infra/s3-front.tf
+++ infra/s3-front.tf
@@ -1,7 +1,7 @@
 module "frontend_s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = "${var.project_name}-frontend"
-  region                   = var.region
+  region = var.region
 
   cors_rule = [
     {
@@ -9,6 +9,15 @@ module "frontend_s3_bucket" {
       allowed_origins = ["https://${var.subdomain}${var.domain}"]
       allowed_headers = ["*"]
       max_age_seconds = 3000
+    }
+  ]
+  lifecycle_rule = [
+    {
+      id      = "expire-noncurrent-versions"
+      enabled = true
+      noncurrent_version_expiration = {
+        days = 30
+      }
     }
   ]
 
