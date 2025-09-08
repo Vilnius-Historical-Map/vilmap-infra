3078ba85e2904334aa99113ef2fde513b5df58b0
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-09-08 13:57:33 +0300

    fix: provide right config for front storage

--- infra/s3-front.tf
+++ infra/s3-front.tf
@@ -1,20 +1,48 @@
 module "frontend_s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
   bucket = "${var.project_name}-frontend"
+  region                   = var.region
 
-  website = {
-    index_document = "index.html"
-    #     error_document = "error.html"
-  }
+  cors_rule = [
+    {
+      allowed_methods = ["GET", "HEAD"]
+      allowed_origins = ["https://${var.subdomain}${var.domain}"]
+      allowed_headers = ["*"]
+      max_age_seconds = 3000
+    }
+  ]
+
+  acl                      = "private"
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
 
   versioning = {
     enabled = true
   }
 
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerEnforced"
-
   force_destroy = true
   tags          = var.tags
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = module.frontend_s3_bucket.s3_bucket_id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${module.frontend_s3_bucket.s3_bucket_arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = module.cloudfront.cloudfront_distribution_arn
+          }
+        }
+      }
+    ]
+  })
 }
