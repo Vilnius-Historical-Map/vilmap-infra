d5f64ef5208fe97e5b3a0d7325b94db0a09925e8
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-09-15 12:02:22 +0300

    refactor: add notification for lambda

--- infra/s3-data.tf
+++ infra/s3-data.tf
@@ -11,3 +11,19 @@ module "data_s3_bucket" {
   force_destroy = true
   tags          = var.tags
 }
+
+
+module "data_s3_notifications" {
+  source = "terraform-aws-modules/s3-bucket/aws//modules/notification"
+
+  bucket = module.data_s3_bucket.s3_bucket_id
+
+  lambda_notifications = {
+    etl = {
+      function_arn  = module.lambda_etl.lambda_function_arn
+      function_name = module.lambda_etl.lambda_function_name
+      events        = ["s3:ObjectCreated:*"]
+      filter_suffix = ".csv"
+    }
+  }
+}
