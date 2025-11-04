0829a80e596aeba439ae29de27f85913aa46e9e0
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-11-04 20:09:56 +0200

    refactor: change name for secrets

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,9 +1,9 @@
 data "aws_ssm_parameter" "db_username" {
-  name            = "/${var.project_name}/prod/db-user"
+  name            = "/${var.project_name}/mvp/db-user"
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "db_password" {
-  name            = "/${var.project_name}/prod/db-password"
+  name            = "/${var.project_name}/mvp/db-password"
   with_decryption = true
 }
